7385a757b07297069063399648e9298380c852df
KirVlas <kirilv@comcast.net>
2014-12-01 18:46:04 -0800

    Initial Commit

--- phonegap/mail.php
+++ phonegap/mail.php
@@ -1,33 +1,30 @@
 <?php
-if(isset($_POST['submit'])) {
-    $to = $_POST["email"];
-    $crust = $_POST["selectedCrust"];
-    $topping = $_POST["selectedTopping"];
-    $drink = $_POST["selectedDrink"];
-    $grandTotal = $_POST["grandTotal"];
-    $subject = "Thanks for your order." . strftime("%T", time());
-    $message .= "Hello,  \n\r";
-    $message .= "You've ordered:\n\r";
-    $message .= "Crust: $crust\n\r";
-    $message .= "Topping: $topping\n\r";
-    $message .= "Drink: $drink\n\r";
-    $message .= "$grandTotal\n\r";
-    $message .= "Thanks again for your order!";
-    mail($to, $subject, $message, $headers);
 
-    $to = "kirilv1984@gmail.com";
-    $message = "";
-    $crust = $_POST["selectedCrust"];
-    $topping = $_POST["selectedTopping"];
-    $drink = $_POST["selectedDrink"];
-    $grandTotal = $_POST["grandTotal"];
-    $subject = "New Order " . strftime("%T", time());
-    $message .= "Crust: $crust\n\r";
-    $message .= "Topping: $topping\n\r";
-    $message .= "Drink: $drink\n\r";
-    $message .= "$grandTotal\n\r";
-    mail($to, $subject, $message, $headers);
+$to = $_POST["email"];
+$crust = $_POST["selectedCrust"];
+$topping = $_POST["selectedTopping"];
+$drink = $_POST["selectedDrink"];
+$grandTotal = $_POST["grandTotal"];
+$subject = "Thanks for your order." . strftime("%T", time());
+$message .= "Hello,  \n\r";
+$message .= "You've ordered:\n\r";
+$message .= "Crust: $crust\n\r";
+$message .= "Topping: $topping\n\r";
+$message .= "Drink: $drink\n\r";
+$message .= "$grandTotal\n\r";
+$message .= "Thanks again for your order!";
+mail($to, $subject, $message, $headers);
 
-    header('Location: index.html');
-}
+$to = "kirilv1984@gmail.com";
+$message = "";
+$crust = $_POST["selectedCrust"];
+$topping = $_POST["selectedTopping"];
+$drink = $_POST["selectedDrink"];
+$grandTotal = $_POST["grandTotal"];
+$subject = "New Order " . strftime("%T", time());
+$message .= "Crust: $crust\n\r";
+$message .= "Topping: $topping\n\r";
+$message .= "Drink: $drink\n\r";
+$message .= "$grandTotal\n\r";
+mail($to, $subject, $message, $headers);
 ?>
